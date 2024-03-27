2d114c47d9ac34abefeb2ddd69bf561b6e8e53a0
matchatco <102745466+matchatco@users.noreply.github.com>
2024-03-27 07:34:41 +0200

    Create ytm3u8.php

---
+++ ytm3u8.php
@@ -0,0 +1,32 @@
+<?php
+
+// YouTube live stream URL
+$youtube_id = $_GET['id'];
+
+//$type_url = $_GET['type'];
+
+$youtube_live_url = "https://www.youtube.com/watch?v=$youtube_id";
+
+// Fetch the live stream URL using cURL
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $youtube_live_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$response = curl_exec($ch);
+
+// Extract the stream URL from the response
+if (preg_match('/"hlsManifestUrl":"(.*?)"/', $response, $matches)) {
+    $hls_manifest_url = $matches[1];
+
+    // Output the HLS manifest URL
+   // echo "HLS manifest URL: " . $hls_manifest_url . "\n";
+    //header("Location: " . $hls_manifest_url);
+     echo $hls_manifest_url;
+
+    // Now you can use $hls_manifest_url to fetch the HLS manifest and process it further
+} else {
+    echo "Error";
+}
+
+curl_close($ch);
+
+?>
